e4614b370892e9f10cb9de4da97c392118e16e8b
YFWang97 <leo.wyf97@gmail.com>
2025-02-22 13:17:56 -0500

    [SW] Updated SW model to include raster part.

--- multi-vertices/sw/reference/src/ref_main.cpp
+++ multi-vertices/sw/reference/src/ref_main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <fstream>
 #include <string>
+#include <vector>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_render.h>
@@ -14,17 +15,71 @@ using namespace std;
 
 #define SDL_ERROR_MSG(...) cout << __VA_ARGS__ << "! " << "SDL_Error: " << SDL_GetError() << endl
 
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
 SDL_Window* gWindow;
 SDL_Renderer* gRenderer;
 
-class Vec3 {
+class Vec {
     public:
-        float vec[3];
+        int size;
+        vector<float> vec;
 
-        Vec3() {
-            for (int i = 0; i < 3; i++) {
+        Vec(int size_) : size(size_), vec(size_) {
+            for (int i = 0; i < size_; i++) {
                 vec[i] = float(0);
             }
+        }
+
+        Vec(const Vec& ref) : size(ref.size), vec(ref.size){
+            for (int i = 0; i < ref.size; i++) {
+                vec[i] = ref.vec[i];
+            }
+        }
+
+        float dot(const Vec& rhs) {
+            float ret = 0;
+
+            for (int i = 0; i < size; i++) {
+                ret += vec[i] * rhs.vec[i];
+            }
+            return ret;
+        }
+
+        void normalize() {
+            float length = 0;
+            for (int i = 0; i < size; i++) {
+                length += vec[i] * vec[i];
+            }
+
+            length = sqrtf(length);
+
+            for (int i = 0; i < size; i++) {
+                vec[i] /= length;
+            }
+        }
+
+        void print(const char* name) {
+            printf("Vector %d: %s\n", size, name);
+            for (int i = 0; i < size; i++) {
+                printf("%f ", vec[i]);
+            }
+            printf("\n");
+        }
+};
+
+class Vec3 : public Vec {
+    public:
+        Vec3() : Vec(3) {}
+
+        Vec3 operator-(const Vec3& rhs) {
+            Vec3 ret;
+            for (int i = 0; i < size; i++) {
+                //printf("%d\n", i);
+                ret.vec[i] = vec[i] - rhs.vec[i];
+            }
+            return ret;
         }
 
         Vec3 cross(Vec3 rhs) {
@@ -35,71 +90,20 @@ class Vec3 {
             ret.vec[2] = vec[0] * rhs.vec[1] - vec[1] * rhs.vec[0];
             return ret;
         }
-
-        float dot(Vec3 rhs) {
-            float ret = 0.0;
-
-            for (int i = 0; i < 3; i++) {
-                ret += vec[i] * rhs.vec[i];
-            }
-
-            return ret;
-        }
-
-        Vec3 operator-(const Vec3& rhs) {
-            Vec3 ret;
-            ret.vec[0] = vec[0] - rhs.vec[0];
-            ret.vec[1] = vec[1] - rhs.vec[1];
-            ret.vec[2] = vec[2] - rhs.vec[2];
-            return ret;
-        }
-
-        void normalize() {
-            float length = sqrtf(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]);
-
-            vec[0] /= length;
-            vec[1] /= length;
-            vec[2] /= length;
-        }
-
-        void print(const char* name) {
-            printf("Vector: %s\n", name);
-            for (int i = 0; i < 3; i++) {
-                printf("%f ", vec[i]);
-            }
-
-            printf("\n");
-        }
 };
 
 
-class Vec4 {
+class Vec4 : public Vec {
     public:
-        float vec[4];
+        Vec4() : Vec(4) {}
 
-
-        Vec4() {
-            for (int i = 0; i < 4; i++) {
-                vec[i] = float(0);
-            }
-        }
-
-        Vec4(Vec3 ref) {
+        Vec4(const Vec3& ref) : Vec(4) {
             for (int i = 0; i < 3; i++) {
                 vec[i] = ref.vec[i];
             }
             vec[3] = float(1);
         }
-
-        void print(const char* name) {
-            printf("Vector: %s\n", name);
-            for (int i = 0; i < 4; i++) {
-                printf("%f ", vec[i]);
-            }
-
-            printf("\n");
-        }
-
+        
         void normalize_to_z() {
             for (int i = 0; i < 4; i++) {
                 vec[i] /= vec[3];
@@ -107,22 +111,21 @@ class Vec4 {
         }
 };
 
-class Vec2 {
+class Vec2 : public Vec {
     public:
-        float vec[2];
+        Vec2() : Vec(2) {}
 
-        Vec2(Vec4 ref) {
-            vec[0] = ref.vec[0];
-            vec[1] = ref.vec[1];
+        Vec2 operator-(const Vec2& rhs) {
+            Vec2 ret;
+            for (int i = 0; i < size; i++) {
+                ret.vec[i] = vec[i] - rhs.vec[i];
+            }
+            return ret;
         }
 
-        void print(const char* name) {
-            printf("Vector: %s\n", name);
-            for (int i = 0; i < 2; i++) {
-                printf("%f ", vec[i]);
-            }
-
-            printf("\n");
+        Vec2(Vec4 ref) : Vec2() {
+            vec[0] = ref.vec[0];
+            vec[1] = ref.vec[1];
         }
 
         void render() {
@@ -131,6 +134,12 @@ class Vec2 {
                     SDL_RenderDrawPoint(gRenderer, i + vec[0], j + vec[1]);
                 } 
             }
+        }
+
+        int cross(Vec2 rhs) {
+            int ret;
+            ret = vec[0] * rhs.vec[1] - vec[1] * rhs.vec[0];
+            return ret;
         }
 };
 
@@ -173,13 +182,50 @@ class Mat4 {
         }
 };
 
+class Mat3 {
+
+    public:
+
+        float mat[3][3];
+
+        Mat3() {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    mat[i][j] = float(0);
+                }
+            }
+        }
+
+        Vec3 dot(Vec3 rhs) {
+            Vec3 ret;
+            for (int i = 0; i < 3; i++) {
+                float accumulation = 0.0;
+                for (int j = 0; j < 3; j++) {
+                    accumulation += mat[i][j] * rhs.vec[j];
+                }
+                ret.vec[i] = accumulation;
+            }
+            return ret;
+        }
+
+        void print(const char* name) {
+            printf("Matrix 3x3: %s\n", name);
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    printf("%f ", mat[i][j]);
+                }
+                printf("\n");
+            }
+            printf("\n");
+        }
+};
 
 
 
 Vec3* vertex_buffer;
 Vec4* vertex_vec4_buffer;
 Vec3* vertex_world_buffer;
-Vec2* result_vetex_buffer;
+Vec2* result_vertex_buffer;
 
 int* index_buffer;
 int* valid_buffer;
@@ -189,6 +235,13 @@ void assembler();
 void vertex_shader();
 
 void rasterizer();
+
+bool is_top_left(Vec2 edge) {
+    bool is_top_edge = edge.vec[1] == 0 && edge.vec[0] > 0;
+    bool is_left_edge = edge.vec[1] < 0;
+
+    return is_left_edge || is_top_edge;
+}
 
 int initialize_sdl() {
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -221,7 +274,12 @@ int initialize_sdl() {
     return 0;
 }
 
+
 int main (void) {
+
+    char color_r[8] = {0xF0, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00};
+    char color_g[8] = {0xF0, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00};
+    char color_b[8] = {0xF0, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00};
 
     const float fovy = 45.0;
     const float n = 1.0;
@@ -229,6 +287,34 @@ int main (void) {
 
     //const float aspect = 640.0/480.0;
     const float aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+
+    float rot_x, rot_y, rot_z;
+
+    rot_x = 0;
+    rot_y = 0;
+    rot_z = 0;
+
+    Mat3 mat_rotation_x_object;
+    Mat3 mat_rotation_y_object;
+    Mat3 mat_rotation_z_object;
+
+    mat_rotation_z_object.mat[0][0] = cos(rot_z * PI / 180.0); 
+    mat_rotation_z_object.mat[0][1] = -sin(rot_z * PI / 180.0); 
+    mat_rotation_z_object.mat[1][0] = sin(rot_z * PI / 180.0); 
+    mat_rotation_z_object.mat[1][1] = cos(rot_z * PI / 180.0); 
+    mat_rotation_z_object.mat[2][2] = float(1);
+
+    mat_rotation_y_object.mat[0][0] = cos(rot_y * PI / 180.0); 
+    mat_rotation_y_object.mat[0][2] = sin(rot_y * PI / 180.0); 
+    mat_rotation_y_object.mat[1][1] = float(1);
+    mat_rotation_y_object.mat[2][0] = -sin(rot_y * PI / 180.0); 
+    mat_rotation_y_object.mat[2][2] = cos(rot_y * PI / 180.0); 
+
+    mat_rotation_x_object.mat[0][0] = float(1);
+    mat_rotation_x_object.mat[1][1] = cos(rot_x * PI / 180.0); 
+    mat_rotation_x_object.mat[1][2] = -sin(rot_x * PI / 180.0); 
+    mat_rotation_x_object.mat[2][1] = sin(rot_x * PI / 180.0); 
+    mat_rotation_x_object.mat[2][2] = cos(rot_x * PI / 180.0); 
 
     float l, r, b, t;
 
@@ -294,7 +380,7 @@ int main (void) {
     mat_rotation_view.mat[2][2] = -camera_g.vec[2];
     mat_rotation_view.mat[3][3] = float(1);
 
-    vertex_buffer = (Vec3*) malloc(8 * sizeof(Vec3));
+    vertex_buffer = new Vec3[8];
 
     ifstream vertices_file("./model/vertices.txt");
 
@@ -312,8 +398,9 @@ int main (void) {
         vertex_index++;
     }
 
+
     index_buffer = (int*) malloc(12 * 3 * sizeof(int)); 
-    
+
     //132
     //031
     //126
@@ -334,14 +421,17 @@ int main (void) {
     }
 
     int index_cnt = 0;
-    
+
     while (!index_file.eof()) {
         index_file >> index_buffer[index_cnt++];
     }
 
-    vertex_vec4_buffer = (Vec4*) malloc(8 * sizeof(Vec4));
+    vertices_file.close();
+    index_file.close();
 
-    vertex_world_buffer = (Vec3*) malloc(8 * sizeof(Vec3));
+    vertex_vec4_buffer = new Vec4[8];
+
+    vertex_world_buffer = new Vec3[8];
 
     Mat4 mat_translation_world;
     mat_translation_world.mat[0][0] = float(1);
@@ -358,18 +448,23 @@ int main (void) {
     mat_viewpoint.mat[1][3] = (SCREEN_HEIGHT - 1.0) / 2.0;
 
 
-    result_vetex_buffer = (Vec2*) malloc(8 * sizeof(Vec2));
+    result_vertex_buffer = new Vec2[8];
 
     string temp;
 
     for (int i = 0; i < 8; i++) {
-        vertex_vec4_buffer[i] = mat_translation_world.dot(Vec4(vertex_buffer[i]));
+        Vec3 vertex_vec3;
+        vertex_vec3 = mat_rotation_x_object.dot(vertex_buffer[i]);
+        vertex_vec3 = mat_rotation_y_object.dot(vertex_vec3);
+        vertex_vec3 = mat_rotation_z_object.dot(vertex_vec3);
+
+        vertex_vec4_buffer[i] = mat_translation_world.dot(Vec4(vertex_vec3));
 
         vertex_vec4_buffer[i] = mat_translation_view.dot(vertex_vec4_buffer[i]);
 
         //temp = "after_view_translation " + to_string(i);
         //vertex_vec4_buffer[i].print(temp.c_str());
-        
+
         vertex_vec4_buffer[i] = mat_rotation_view.dot(vertex_vec4_buffer[i]);
 
         //temp = "after_view_rotation " + to_string(i);
@@ -393,14 +488,10 @@ int main (void) {
 
         Vec2 result(vertex_vec4_buffer[i]);
 
-        result_vetex_buffer[i] = result;
+        result_vertex_buffer[i] = result;
     }     
 
     valid_buffer = (int*) malloc(12 * sizeof(int));
-
-    for (int i = 0; i < 12; i++) {
-        valid_buffer[i] = 1;
-    }
 
     for (int i = 0; i < 12; i++) {
         Vec3 vec01 = vertex_world_buffer[index_buffer[3 * i + 1]] - vertex_world_buffer[index_buffer[3 * i]];
@@ -413,6 +504,74 @@ int main (void) {
         printf("Debug: Tri %d is %d\n", i, valid_buffer[i]);
     }
 
+    char frame_buffer[640*480][3];
+
+    memset(frame_buffer, 0xFF, 640*480*3); 
+
+    for (int i = 0; i < 12; i++) {
+        if (valid_buffer[i] == 0) continue;
+
+        Vec2 v0(result_vertex_buffer[index_buffer[3 * i]]);
+        Vec2 v1(result_vertex_buffer[index_buffer[3 * i + 1]]);
+        Vec2 v2(result_vertex_buffer[index_buffer[3 * i + 2]]);
+
+        int x_min = floor(MIN(MIN(v0.vec[0], v1.vec[0]), v2.vec[0]));
+        int y_min = floor(MIN(MIN(v0.vec[1], v1.vec[1]), v2.vec[1]));
+        int x_max = ceil(MAX(MAX(v0.vec[0], v1.vec[0]), v2.vec[0]));
+        int y_max = ceil(MAX(MAX(v0.vec[1], v1.vec[1]), v2.vec[1]));        
+
+        Vec2 edge_w0, edge_w1, edge_w2;
+
+        edge_w0 = v2 - v1;
+        edge_w1 = v0 - v2;
+        edge_w2 = v1 - v0;
+
+        // Compute the constant delta_s that will be used for the horizontal and vertical steps
+        int delta_w0_col = -edge_w0.vec[1];
+        int delta_w1_col = -edge_w1.vec[1];
+        int delta_w2_col = -edge_w2.vec[1];
+        int delta_w0_row = edge_w0.vec[0];
+        int delta_w1_row = edge_w1.vec[0];
+        int delta_w2_row = edge_w2.vec[0];
+
+        //// Rasterization fill rule, not 100% precise due to floating point innacuracy
+        //float bias0 = is_top_left(edge_w0) ? 0 : -0.0001;
+        //float bias1 = is_top_left(edge_w1) ? 0 : -0.0001;
+        //float bias2 = is_top_left(edge_w2) ? 0 : -0.0001;
+
+        // Compute the edge functions for the fist (top-left) point
+        Vec2 p0;
+        p0.vec[0] = x_min;
+        p0.vec[1] = y_min;
+
+        int w0_row = edge_w0.cross(p0-v1);
+        int w1_row = edge_w1.cross(p0-v2);
+        int w2_row = edge_w2.cross(p0-v0);
+
+        for (int y = y_min; y <= y_max; y++) {
+            int w0 = w0_row;
+            int w1 = w1_row;
+            int w2 = w2_row;
+
+            for (int x = x_min; x <= x_max; x++) {
+                bool is_inside = (w0 < 0 && w1 < 0 && w2 < 0);
+
+                if (is_inside) {
+                    frame_buffer[x + y * 640][0] = color_r[i];
+                    frame_buffer[x + y * 640][1] = color_g[i];
+                    frame_buffer[x + y * 640][2] = color_b[i];
+                }
+
+                w0 += delta_w0_col;
+                w1 += delta_w1_col;
+                w2 += delta_w2_col;
+            }
+
+            w0_row += delta_w0_row;
+            w1_row += delta_w1_row;
+            w2_row += delta_w2_row;
+        }
+    }
 
     initialize_sdl();
 
@@ -428,7 +587,7 @@ int main (void) {
                 if (event.type == SDL_KEYDOWN) {
                     switch(event.key.keysym.sym) {
                         case SDLK_DOWN:
-                            
+
                             break;
                     }
                 }
@@ -436,18 +595,20 @@ int main (void) {
         }
         SDL_RenderClear(gRenderer);
 
-        char r[8] = {0xF0, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00};
-        char g[8] = {0xF0, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00};
-        char b[8] = {0xF0, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00};
+        for (int y = 0; y < 480; y++) {
+            for (int x = 0; x < 640; x++) {
+                SDL_SetRenderDrawColor(gRenderer, frame_buffer[x + y * 640][0], frame_buffer[x + y * 640][1], frame_buffer[x + y * 640][2], 0xFF);
+                SDL_RenderDrawPoint(gRenderer, x, y);
+            }
+        }
+
 
         for (int i = 0; i < 8; i++) {
-            SDL_SetRenderDrawColor(gRenderer, r[i], g[i], b[i], 0xFF);
-            result_vetex_buffer[i].render();
-
+            SDL_SetRenderDrawColor(gRenderer, color_r[i], color_g[i], color_b[i], 0xFF);
+            result_vertex_buffer[i].render();
         }
 
         SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
 
         SDL_RenderPresent(gRenderer);
     }
